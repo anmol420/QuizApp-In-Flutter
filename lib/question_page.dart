0728f981117f1ffc50c875627cd27021277fe843
Anmol Anand <76034132+anmol420@users.noreply.github.com>
2023-05-21 16:51:52 +0530

    Added Custom ButtonWidget

--- lib/question_page.dart
+++ lib/question_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class QuestionPage extends StatefulWidget {
   const QuestionPage({super.key});
@@ -12,6 +13,34 @@ class QuestionPage extends StatefulWidget {
 class _QuestionPageState extends State<QuestionPage> {
   @override
   Widget build(context) {
-    return const Text('QuestionScreen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'The Question .....',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          AnswerButton(
+            'Answer....1',
+            () {},
+          ),
+          AnswerButton(
+            'Answer....2',
+            () {},
+          ),
+          AnswerButton(
+            'Answer....3',
+            () {},
+          ),
+        ],
+      ),
+    );
   }
 }
