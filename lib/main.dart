eadc24218cf77b7011b44c9d242ab21b39c327ed
Anmol Anand <76034132+anmol420@users.noreply.github.com>
2023-06-13 16:34:08 +0530

    Restructuring

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/quiz.dart';
+import 'package:quiz_app/start_screen.dart';
 
 void main() {
   runApp(
-    const Quiz(),
+    const MaterialApp(
+      home: Scaffold(
+        body: StartScreen(),
+      ),
+    ),
   );
 }
